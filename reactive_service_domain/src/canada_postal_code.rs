c3df7bd83af46ccf6d955f92583d54a29c11e8da
Nicolas Riche <nicolas.vincent.riche@gmail.com>
2024-03-20 14:02:27 -0400

    Use FromStr instead of TryFrom (Semantic difference)

--- reactive_service_domain/src/canada_postal_code.rs
+++ reactive_service_domain/src/canada_postal_code.rs
@@ -20,29 +20,28 @@ pub enum PostalCodeError {
 /// Display implementation prints the postal code with the middle space: A1A 0B0
 ///
 /// # Examples
-///
 /// ```
 /// # use std::convert::TryFrom;
 /// # use reactive_service_domain::canada_postal_code::CanadaPostalCode;
-/// assert_eq!(CanadaPostalCode::try_from("A1A 0B0").is_ok(), true);
-/// assert_eq!(CanadaPostalCode::try_from("A1A0B0").is_ok(), true);
+/// assert_eq!("A1A 0B0".parse::<CanadaPostalCode>().is_ok(), true);
+/// assert_eq!("A1A0B0".parse::<CanadaPostalCode>().is_ok(), true);
+/// assert_eq!("a1a 0b0".parse::<CanadaPostalCode>().is_ok(), true);
 ///
-/// assert_eq!(CanadaPostalCode::try_from("a1a 0b0").is_ok(), true);
+/// assert_eq!("A1A".parse::<CanadaPostalCode>().is_err(), true);
+/// assert_eq!("A1A 0B0 A1A".parse::<CanadaPostalCode>().is_err(), true);
+/// assert_eq!("21111".parse::<CanadaPostalCode>().is_err(), true);
 /// 
-/// assert_eq!(CanadaPostalCode::try_from("A1A").is_err(), true);
-/// assert_eq!(CanadaPostalCode::try_from("A1A 0B0 A1A").is_err(), true);
-/// assert_eq!(CanadaPostalCode::try_from("21111").is_err(), true);
-/// assert_eq!(format!("{}", CanadaPostalCode::try_from("A1A 0B0").unwrap()), "A1A 0B0");
-/// assert_eq!(format!("{}", CanadaPostalCode::try_from("A1A0B0").unwrap()), "A1A 0B0");
+/// assert_eq!(format!("{}", "A1A 0B0".parse::<CanadaPostalCode>().unwrap()), "A1A 0B0");
+/// assert_eq!(format!("{}", "A1A0B0".parse::<CanadaPostalCode>().unwrap()), "A1A 0B0");
 /// ```
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct CanadaPostalCode(heapless::String<6>);
 
-impl TryFrom<&str> for CanadaPostalCode {
-    type Error = PostalCodeError;
+impl FromStr for CanadaPostalCode {
+    type Err = PostalCodeError;
 
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        let sanitized = value.trim().to_uppercase();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let sanitized = s.trim().to_uppercase();
 
         if CANADA_POSTAL_CODE.is_match(&sanitized) {
             let no_space = sanitized.replace(' ', "");
