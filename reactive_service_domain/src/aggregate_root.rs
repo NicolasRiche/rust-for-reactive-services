3a96b6a07ec4d565ee8c021f89d24a0c40cadff5
Nicolas Riche <nicolas.vincent.riche@gmail.com>
2024-04-04 10:57:24 -0400

    clean trait

--- reactive_service_domain/src/aggregate_root.rs
+++ reactive_service_domain/src/aggregate_root.rs
@@ -4,15 +4,21 @@ pub struct SequencedEvent<E> {
     pub event: E,
 }
 
+/// Define the entry point for interactions with an Entity
 pub trait AggregateRoot {
     type State;
     type Command;
     type Error;
     type Event;
 
-    fn load_from_events(&mut self, events: Vec<SequencedEvent<Self::Event>>)
+    /// Create the entity from historical events.
+    /// Return the read only state of the entity after applying the events.
+    fn from_events(&mut self, events: Vec<SequencedEvent<Self::Event>>)
         -> Result<&Self::State, Self::Error>;
 
+    /// Handle a command
+    /// Success: Return the updated read only state + the sequence of applied events.
+    /// Failure: Return an error, the state is unchanged. 
     fn handle_command(&mut self, command: Self::Command) 
         -> Result<(&Self::State, Vec<SequencedEvent<Self::Event>>), Self::Error>;
 }
