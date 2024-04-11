43923969d81777e185668c2286f9ab982c439357
Nicolas Riche <nicolas.vincent.riche@gmail.com>
2024-04-11 09:34:38 -0400

    tax and shipping calc as trait

--- reactive_service_domain/src/shipping_calculator.rs
+++ reactive_service_domain/src/shipping_calculator.rs
@@ -1,10 +1,14 @@
 use crate::non_empty_cart::NonEmptyCart;
 use crate::order_state::{Currency, DeliveryAddress, Money};
 
-pub struct ShippingCalculator {}
+pub trait ShippingCalculator {
+    fn shipping_cost(&self, cart: &NonEmptyCart, delivery_address: &DeliveryAddress) -> Money;
+}
 
-impl ShippingCalculator {
-    pub fn shipping_cost(&self, cart: &NonEmptyCart, delivery_address: &DeliveryAddress) -> Money {
+pub struct LocalShippingCalculator {}
+
+impl ShippingCalculator for LocalShippingCalculator {
+    fn shipping_cost(&self, cart: &NonEmptyCart, delivery_address: &DeliveryAddress) -> Money {
         let _ = cart;
         let _ = delivery_address;
         Money {
--- reactive_service_domain/src/tax_calculator.rs
+++ reactive_service_domain/src/tax_calculator.rs
@@ -1,10 +1,14 @@
 use crate::non_empty_cart::NonEmptyCart;
 use crate::order_state::{Currency, Money};
 
-pub struct TaxCalculator {}
 
-impl TaxCalculator {
-    pub fn tax_cost(&self, cart: &NonEmptyCart, shipping_cost: &Money) -> Money {
+pub trait TaxCalculator {
+    fn tax_cost(&self, cart: &NonEmptyCart, shipping_cost: &Money) -> Money;
+}
+pub struct LocalTaxCalculator {}
+
+impl TaxCalculator for LocalTaxCalculator {
+    fn tax_cost(&self, cart: &NonEmptyCart, shipping_cost: &Money) -> Money {
         let _ = cart;
         let _ = shipping_cost;
         Money {
